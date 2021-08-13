8b02a21c5dc0e5ff6fb76a1b7a0f974273aae444
zhaiyl07 <matkerimov.zhaiyl.ib9.1.19@gmail.com>
2021-08-14 01:50:39 +0600

    axios, chalk

---
+++ script.js
@@ -0,0 +1,42 @@
+// ------------------ 14/08/2021
+// ------------------------------------- chalk
+// ------------------------------------- axios
+
+// const chalk = require('chalk');
+// const axios = require('axios')
+//
+// axios("https://jsonplaceholder.typicode.com/users/2")
+//     .then(result => console.log(result.data))
+//     .catch(() => console.log("Error"))
+
+// console.log(chalk.magenta('Hello world!'));
+// console.log(chalk.cyan('Hello world!'));
+// console.log(chalk.redBright('Hello world!'));
+// console.log(chalk.yellowBright('Hello world!'));
+// console.log(chalk.blueBright('Hello world!'));
+// console.log(chalk.magentaBright('Hello world!'));
+// console.log(chalk.cyanBright('Hello world!'));
+// console.log(chalk.whiteBright('Hello world!'));
+
+// const newContact = {
+//     name: "Taalai",
+//     phone: "0709-934-305"
+// }
+
+// axios.post("https://611675a31c592d0017bb7f05.mockapi.io/users", newContact)
+//     .then(result => console.log(result.data))
+
+// const update = {phone: "937996"}
+// axios.put("https://611675a31c592d0017bb7f05.mockapi.io/users/2", update)
+//     .then(result => console.log(result.data))
+
+// axios.delete("https://611675a31c592d0017bb7f05.mockapi.io/users/2")
+//     .then(result => console.log(result.data))
+
+// --------------------------------------------------------------------------------
+// const terminal = (num) =>{
+//     return
+// }
+// console.log(terminal(357))
+
+
